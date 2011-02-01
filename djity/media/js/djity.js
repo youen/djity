1599c42165c7bf9f897b302fbe1eaf6bd65ce681
Youen Péron <youen.peron@gmail.com>
2011-02-01 18:12:25 +0100

    Notify widget

--- djity/media/js/djity.js
+++ djity/media/js/djity.js
@@ -519,7 +519,7 @@ function widgetify() {
 			}
 	);
 
-	$('#messages').children('div').box();
+	$('#messages').children('div').notify();
 
 	if(edit_perm){
 		$(".dj-editable").each(function(i,e){$(e).editable({save_function:eval(e.id +'_callback')});});
@@ -762,7 +762,7 @@ function init_tag(){
 function message(msg) {
 	msg_div = $('<div class="ui-helper-hidden" ><p>'+msg +'</p></div>');
 	$('#messages').prepend(msg_div);
-	msg_div.box({closeable:true,effect:'slide',delay:10000});
+	msg_div.notify({expires:10000});
 
 }
 
@@ -931,11 +931,6 @@ $.widget("ui.editable",{
 						.appendTo(editorBox);
 			});
 
-			editorBox.position({
-						my:'left bottom',
-						at:'left top',
-						of:self.element,
-					 })
 
 					
     },
@@ -973,6 +968,13 @@ $.widget("ui.editable",{
 				options = self.options,
 				
 				editorBox = self.editorBox;
+			
+			editorBox.position({
+						my:'left bottom',
+						at:'left top',
+						of:self.doc,
+					 });
+			
 			editorBox
 				.show(options.effect);
 					
@@ -1068,3 +1070,143 @@ $.widget("ui.lefttabs", {
 });
 
 
+/*
+* jQuery Notify UI Widget 1.4
+* Copyright (c) 2010 Eric Hynds
+*
+* http://www.erichynds.com/jquery/a-jquery-ui-growl-ubuntu-notification-widget/
+*
+* Depends:
+* - jQuery 1.4
+* - jQuery UI 1.8 widget factory
+*
+* Dual licensed under the MIT and GPL licenses:
+* http://www.opensource.org/licenses/mit-license.php
+* http://www.gnu.org/licenses/gpl.html
+*
+*/
+(function($){
+
+$.widget("ech.notify", {
+options: {
+speed: 500,
+expires: 5000,
+stack: 'below',
+custom: false
+},
+_create: function(){
+var self = this;
+this.templates = {};
+this.keys = [];
+
+// build and save templates
+this.element.addClass("ui-notify").children().addClass("ui-notify-message ui-notify-message-style").each(function(i){
+var key = this.id || i;
+self.keys.push(key);
+self.templates[key] = $(this).removeAttr("id").wrap("<div></div>").parent().html(); // because $(this).andSelf().html() no workie
+}).end().empty().show();
+},
+create: function(template, msg, opts){
+if(typeof template === "object"){
+opts = msg;
+msg = template;
+template = null;
+}
+
+var tpl = this.templates[ template || this.keys[0]];
+
+// remove default styling class if rolling w/ custom classes
+if(opts && opts.custom){
+tpl = $(tpl).removeClass("ui-notify-message-style").wrap("<div></div>").parent().html();
+}
+
+// return a new notification instance
+return new $.ech.notify.instance(this)._create(msg, $.extend({}, this.options, opts), tpl);
+}
+});
+
+// instance constructor
+$.extend($.ech.notify, {
+instance: function(widget){
+this.parent = widget;
+this.isOpen = false;
+}
+});
+
+// instance methods
+$.extend($.ech.notify.instance.prototype, {
+_create: function(params, options, template){
+this.options = options;
+
+var self = this,
+
+// build html template
+html = template.replace(/#(?:\{|%7B)(.*?)(?:\}|%7D)/g, function($1, $2){
+return ($2 in params) ? params[$2] : '';
+}),
+
+// the actual message
+m = (this.element = $(html)),
+
+// close link
+closelink = m.find(".ui-notify-close");
+
+// clickable?
+if(typeof this.options.click === "function"){
+m.addClass("ui-notify-click").bind("click", function(e){
+self._trigger("click", e, self);
+});
+}
+
+// show close link?
+if(closelink.length){
+closelink.bind("click", function(){
+self.close();
+return false;
+});
+}
+
+this.open();
+
+// auto expire?
+if(typeof options.expires === "number"){
+window.setTimeout(function(){
+self.close();
+}, options.expires);
+}
+
+return this;
+},
+close: function(){
+var self = this, speed = this.options.speed;
+
+this.element.fadeTo(speed, 0).slideUp(speed, function(){
+self._trigger("close");
+self.isOpen = false;
+});
+
+return this;
+},
+open: function(){
+if(this.isOpen || this._trigger("beforeopen") === false){
+return this;
+}
+
+var self = this;
+
+this.element[this.options.stack === 'above' ? 'prependTo' : 'appendTo'](this.parent.element).css({ display:"none", opacity:"" }).fadeIn(this.options.speed, function(){
+self._trigger("open");
+self.isOpen = true;
+});
+
+return this;
+},
+widget: function(){
+return this.element;
+},
+_trigger: function(type, e, instance){
+return this.parent._trigger.call( this, type, e, instance );
+}
+});
+
+})(jQuery);
