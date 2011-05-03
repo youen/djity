48c662cfb124c7a7d84ff18a3fb095554358a5f9
Youen Péron <youen.peron@gmail.com>
2011-05-03 22:02:26 +0200

    change login form to enable the "save password" function of brothers

--- djity/media/js/widgets/login.js
+++ djity/media/js/widgets/login.js
@@ -1,12 +1,3 @@
-var login_dialog_html = 
-		'<div id="login_dialog" class="ui-helper-hidden" title="' +gettext('Login') +'">' + 
-			'<form method="post" action="." class="">' +
-				'<label for="login_username">' + gettext("user")  + ' </label><br/>' + 
-				'<input id="login_username" name="username" type="text"><br/>' +
-				'<label for="login_password">' + gettext("password") + ' </label><br/>' + 
-				'<input id="login_password" name="password" type="password"><br/>' +
-			'</form>'+
-		'</div>';
 
 
 dj.widgets.login = 
@@ -29,7 +20,7 @@ dj.widgets.login =
 			})
 			.addClass('dj-mini-button');
 
-		this.dialog = $(login_dialog_html)
+		this.dialog = $('#login_dialog')
 			.dialog(
 			{
 				autoOpen:false,
