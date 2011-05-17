ebac4675ee6e13ec180bea86b215425f135f35e9
Youen Péron <youen.peron@gmail.com>
2011-05-17 14:57:12 +0200

    start test for djity

--- djity/project/tests.py
+++ djity/project/tests.py
@@ -1,23 +1,43 @@
+# vim: tabstop=4 expandtab shiftwidth=4 softtabstop=4
 """
 This file demonstrates two different styles of tests (one doctest and one
 unittest). These will both pass when you run "manage.py test".
 
 Replace these with more appropriate tests for your application.
 """
+import json
+from django.utils import unittest
+from django.test.client import Client
+from djity.project.models import Project
+import djity 
 
-from django.test import TestCase
+class ProjectTest(unittest.TestCase):
+    fixtures = [djity.__path__[0]+'/fixtures/test_project.json']
 
-class SimpleTest(TestCase):
-    def test_basic_addition(self):
+    def setUp(self):
+        # Every test needs a client.
+        self.client = Client()
+
+
+    def test_login(self):
         """
-        Tests that 1 + 1 always equals 2.
+        Test the login
         """
-        self.failUnlessEqual(1 + 1, 2)
+        argv = {
+                'js_traget':'document',
+                'path':'',
+                'username':'admin',
+                'password':'admin',
+                'project_name':'root',
+                'module_name':'home',
+                'LANGUAGE_CODE':'fr'
+                }
+        Project.objects.get(name='root')
+        response = self.client.post('/dajaxice/djity.portal.login/',{'callback':'Dajax.process','argv':json.dumps(argv)})
+        self.assertEqual(response.status_code, 200)
+        self.assertEqual(response.context['username'], 'admin')
 
-__test__ = {"doctest": """
-Another way to test that 1 + 1 is equal to 2.
+        
 
->>> 1 + 1 == 2
-True
-"""}
+
 
