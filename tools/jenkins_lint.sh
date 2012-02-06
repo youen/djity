2601a1c1388170e858b8ca5a87c08f946ce9441e
Youen Peron <youen.peron@gmail.com>
2012-02-06 16:07:54 +0100

    start continuous integration

---
+++ tools/jenkins_lint.sh
@@ -0,0 +1,7 @@
+
+pylint --rcfile ${WORKSPACE}/tools/pylintrc -f parseable `find djity  -name "*.py"` > ${WORKSPACE}/pylint.txt
+echo "pylint complete"
+
+java -jar ${WORKSPACE}/tools/jslint4java-2.0.1.jar --undef --maxerr 300 --report xml ${WOKSPACE}djity/static/djity/js/djity.js ${WORKSPACE}/djity/static/djity/js/widgets/*.js  > ${WORKSPACE}/jslint.xml
+echo "jslint complete"
+
---
+++ tools/jenkins_setup.sh
@@ -0,0 +1,8 @@
+
+
+pip install -r dev-req.txt
+
+python setup.py develop
+
+
+
