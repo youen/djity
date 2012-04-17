3e213327ccdfd64d47331e2cafba4b0f21fe038a
Youen <youen.peron@gmail.com>
2012-04-17 22:38:09 +0200

    get site-package for pil and mysql

--- tools/jenkins_virtualenv.sh
+++ tools/jenkins_virtualenv.sh
@@ -4,7 +4,7 @@ if [ -d "${WORKSPACE}/.env" ]; then
 	echo "**> Virtualenv exists"
 else
 	echo "**> Creating virtualenv"
-	virtualenv ${WORKSPACE}/.env
+	virtualenv --system-site-packages ${WORKSPACE}/.env
 	echo "**> Creating PYTHON_EGG_CACHE"
 	mkdir ${WORKSPACE}/.env/PYTHON_EGG_CACHE
 fi
