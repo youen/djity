35852994f7c760e7eb97b18f74a1279377c2dd64
Youen <youen.peron@gmail.com>
2012-04-19 07:30:57 +0200

    purge the previous project

--- tools/jenkins_deployment.sh
+++ tools/jenkins_deployment.sh
@@ -1,3 +1,5 @@
+#!/bin/sh -xe
+
 . ${WORKSPACE}/tools/jenkins_virtualenv.sh
 
 ROOTDIR=${WORKSPACE}/../
@@ -6,6 +8,8 @@ echo "change directory to ${ROOTDIR}"
 cd ${ROOTDIR}
 
 ROOTDIR=`pwd`
+
+rm -rf ${ROOTDIR}/${JOB_NAME}
 
 echo "create new project"
 echo "djity-admin.py create_project ${ROOTDIR}/${JOB_NAME} "
